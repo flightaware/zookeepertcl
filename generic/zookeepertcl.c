e5609b7d9ac5272dc37c0b30321c048d95e45d9c
Karl Lehenbauer <karl-gh@lehenbauer.com>
2016-12-30 00:11:06 +0000

    further flesh out tcl interface to zookeeper

--- generic/zookeepertcl.c
+++ generic/zookeepertcl.c
@@ -15,32 +15,6 @@
 /*
  *--------------------------------------------------------------
  *
- * zookeepertcl_zookeeperObjectDelete -- command deletion callback routine.
- *
- * Results:
- *      ...destroys the zookeeper object.
- *      ...frees memory.
- *
- * Side effects:
- *      None.
- *
- *--------------------------------------------------------------
- */
-void
-zookeepertcl_zookeeperObjectDelete (ClientData clientData)
-{
-    zookeepertcl_objectClientData *zo = (zookeepertcl_objectClientData *)clientData;
-
-    assert (zo->zookeeper_object_magic == ZOOKEEPER_OBJECT_MAGIC);
-
-	// destroy stuff here
-    ckfree((char *)clientData);
-}
-
-
-/*
- *--------------------------------------------------------------
- *
  * zookeeper_state_to_string -- given a zookeeper
  *   state code return a string corresponding
  *
@@ -104,6 +78,158 @@ const char *zookeeper_type_to_string (int state)
 
 
 /*
+ *--------------------------------------------------------------
+ *
+ * zookeeper_error_to_code_string -- given a zookeeper error
+ *   return a corresponding string
+ *
+ * Results:
+ *      returns a pointer to a const char *
+ *
+ * Side effects:
+ *      None.
+ *
+ *--------------------------------------------------------------
+ */
+const char *zookeeper_error_to_code_string (int status)
+{
+	switch (status) {
+		case ZOK:
+			return "ZOK";
+
+		case ZSYSTEMERROR:
+			return "ZSYSTEMERROR";
+
+		case ZRUNTIMEINCONSISTENCY:
+			return "ZRUNTIMEINCONSISTENCY";
+
+		case ZDATAINCONSISTENCY:
+			return "ZDATAINCONSISTENCY";
+
+		case ZCONNECTIONLOSS:
+			return "ZCONNECTIONLOSS";
+
+		case ZMARSHALLINGERROR:
+			return "ZMARSHALLINGERROR";
+
+		case ZUNIMPLEMENTED:
+			return "ZUNIMPLEMENTED";
+
+		case ZOPERATIONTIMEOUT:
+			return "ZOPERATIONTIMEOUT";
+
+		case ZBADARGUMENTS:
+			return "ZBADARGUMENTS";
+
+		case ZINVALIDSTATE:
+			return "ZINVALIDSTATE";
+
+		case ZAPIERROR:
+			return "ZAPIERROR";
+
+		case ZNONODE:
+			return "ZNONODE";
+
+		case ZNOAUTH:
+			return "ZNOAUTH";
+
+		case ZBADVERSION:
+			return "ZBADVERSION";
+
+		case ZNOCHILDRENFOREPHEMERALS:
+			return "ZNOCHILDRENFOREPHEMERALS";
+
+		case ZNODEEXISTS:
+			return "ZNODEEXISTS";
+
+		case ZNOTEMPTY:
+			return "ZNOTEMPTY";
+
+		case ZSESSIONEXPIRED:
+			return "ZSESSIONEXPIRED";
+
+		case ZINVALIDCALLBACK:
+			return "ZINVALIDCALLBACK";
+
+		case ZINVALIDACL:
+			return "ZINVALIDACL";
+
+		case ZAUTHFAILED:
+			return "ZAUTHFAILED";
+
+		case ZCLOSING:
+			return "ZCLOSING";
+
+		case ZNOTHING:
+			return "ZNOTHING";
+
+		case ZSESSIONMOVED:
+			return "ZSESSIONMOVED";
+
+		default:
+			return "ZUNKNOWN";
+	}
+}
+
+/*
+ *--------------------------------------------------------------
+ *
+ * zookeeper_set_tcl_return_code -- given a zookeeper status
+ *   return TCL_OK if it's ZOK else set an error result and
+ *   return TCL_ERROR
+ *
+ * Results:
+ *      returns TCL_OK or TCL_ERROR
+ *
+ * Side effects:
+ *      May set an error message into the Tcl result and
+ *      set errorCode.
+ *
+ *--------------------------------------------------------------
+ */
+int
+zookeeper_set_tcl_return_code (int status) {
+	if (status == ZOK) {
+		return TCL_OK;
+	}
+
+	// NB this needs to be spruced up to set errorCode and a
+	// better error message and stuff
+	Tcl_SetObjResult (interp, Tcl_NewStringObj (stateString, -1));
+	return TCL_ERROR;
+}
+
+/*
+ *--------------------------------------------------------------
+ *
+ * zookeepertcl_zookeeperObjectDelete -- command deletion callback routine.
+ *
+ * Results:
+ *      ...destroys the zookeeper object.
+ *      ...frees memory.
+ *
+ * Side effects:
+ *      None.
+ *
+ *--------------------------------------------------------------
+ */
+void
+zookeepertcl_zookeeperObjectDelete (ClientData clientData)
+{
+    zookeepertcl_objectClientData *zo = (zookeepertcl_objectClientData *)clientData;
+
+    assert (zo->zookeeper_object_magic == ZOOKEEPER_OBJECT_MAGIC);
+
+	int status = zoopeeker_close (zo->zh);
+	if (status != ZOK) {
+		char *errorString = zookeeper_error_to_string (status);
+	}
+
+	// destroy stuff here
+    ckfree((char *)clientData);
+}
+
+/*
  *----------------------------------------------------------------------
  *
  * zookeeper_EventSetupProc --
@@ -145,6 +271,132 @@ void
 zookeeper_EventCheckProc (ClientData clientData, int flags) {
     zookeepertcl_objectClientData *zo = (zookeepertcl_objectClientData *)clientData;
 
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * zookeepertcl_zookeeperObjectObjCmd --
+ *
+ *      perform methods of a zookeeper object
+ *
+ * Results:
+ *      A standard Tcl result.
+ *
+ *
+ *----------------------------------------------------------------------
+ */
+
+    /* ARGSUSED */
+int
+zookeepertcl_zookeeperObjectObjCmd(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
+{
+    zookeepertcl_objectClientData *zo = (zookeepertcl_objectClientData *)clientData;
+	ZOOAPI zhandle_t *zh = zo->zh;
+
+    static CONST char *options[] = {
+        "exists",
+        "delete",
+        "state",
+        "recv_timeout",
+        "is_unrecoverable",
+        NULL
+    };
+
+    enum options {
+		OPT_EXISTS,
+		OPT_DELETE,
+        OPT_STATE,
+		OPT_RECV_TIMEOUT,
+		OPT_IS_UNRECOVERABLE
+    };
+
+    // basic command line processing
+    if (objc < 2) {
+        Tcl_WrongNumArgs (interp, 1, objv, "subcommand ?args?");
+        return TCL_ERROR;
+    }
+
+    // argument must be one of the subOptions defined above
+    if (Tcl_GetIndexFromObj (interp, objv[1], options, "option",
+        TCL_EXACT, &optIndex) != TCL_OK) {
+        return TCL_ERROR;
+    }
+
+    switch ((enum options) optIndex) {
+		case OPT_EXISTS:
+		{
+			char *path;
+			int watch = 0;
+			struct Stat stat;
+
+			if (objc != 4) {
+				Tcl_WrongNumArgs (interp, 2, objv, "path watch");
+				return TCL_ERROR;
+			}
+
+			path = Tcl_GetString (objv[2]);
+
+			if (Tcl_GetBooleanFromObj (interp, objv[3], &watch) == TCL_ERROR) {
+				return TCL_ERROR;
+			}
+
+			int status = zoo_exists (zh, path, watch, &stat);
+			return zookeeper_set_tcl_return_code (status);
+		}
+
+		case OPT_DELETE:
+		{
+			char *path;
+			int version = 0;
+
+			if (objc != 4) {
+				Tcl_WrongNumArgs (interp, 2, objv, "path version");
+				return TCL_ERROR;
+			}
+
+			path = Tcl_GetString (objv[2]);
+
+			if (Tcl_GetIntFromObj (interp, objv[3], &version) == TCL_ERROR) {
+				return TCL_ERROR;
+			}
+
+			int status = zoo_delete (zh, path, version);
+			return zookeeper_set_tcl_return_code (status);
+		}
+
+		case OPT_STATE:
+		{
+			if (objc != 2) {
+				Tcl_WrongNumArgs (interp, 2, objv, "");
+				return TCL_ERROR;
+			}
+
+			int state = zoo_state (zh);
+			const char *stateString = zookeeper_state_to_string (state);
+			Tcl_SetObjResult (interp, Tcl_NewStringObj (stateString, -1));
+			break;
+		}
+
+		case OPT_RECV_TIMEOUT:
+		{
+			if (objc != 2) {
+				Tcl_WrongNumArgs (interp, 2, objv, "");
+				return TCL_ERROR;
+			}
+
+			Tcl_SetObjResult (interp, Tcl_NewIntObj (zoo_recv_timeout (zh)));
+			break;
+		}
+
+		case OPT_IS_UNRECOVERABLE:
+		{
+			Tcl_SetObjResult (interp, Tcl_NewBooleanObj (is_recoverable (zh) == ZINVALIDSTATE));
+			break;
+		}
+	}
+
+    return TCL_OK;
 }
 
 /*
@@ -248,7 +500,7 @@ zookeepertcl_zookeeperObjCmd(ClientData clientData, Tcl_Interp *interp, int objc
 			}
 
 			// create a Tcl command to interface to zookeeper
-			zo->cmdToken = Tcl_CreateObjCommand (interp, cmdName, zookeepertcl_zookeeperObjCmd, zo, zookeepertcl_zookeeperObjectDelete);
+			zo->cmdToken = Tcl_CreateObjCommand (interp, cmdName, zookeepertcl_zookeeperObjectObjCmd, zo, zookeepertcl_zookeeperObjectDelete);
 			Tcl_SetObjResult (interp, Tcl_NewStringObj (cmdName, -1));
 			if (autoGeneratedName == 1) {
 				ckfree(cmdName);
